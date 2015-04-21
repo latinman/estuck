591fb6ee9aa242d44a0de54945cbb2b1241bff7c
Ernesto Rivas <erivas78@gmail.com>
2015-04-21 15:40:39 -0400

    Created Dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!
+
   def home
     if current_user
       redirect_to books_path
     end
+  end
+
+  def dashboard
+    @book = current_user.books
   end
 end
